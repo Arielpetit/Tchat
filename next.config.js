13c40b3c046ee912d861f1dbf9de94915ea769b7
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-13 10:56:15 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})
 
 module.exports = withPWA({
-
-});
+	reactStrictMode: true,
+})
